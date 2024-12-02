57d2016d7fab7a235656d03286011e6fd2e84726
VladosLavan <vladborrrrisov@gmail.com>
2024-12-02 12:00:37 +0300

    1) highlighted separate functions for selecting categories and units 2) corrected the comments on the call 3) A new category of VolumeRations has been added

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
-import inspect
-from unit_converter import UnitConverter, Pressure, VolumeFlowRate, UnitCategory, output_result
+from unit_converter import UnitConverter, PressureUnitCategory, VolumeFlowRateUnitCategory, VolumeRationsUnitCategory, \
+    output_result
 
 
-def main():
-    categories = [cls for _, cls in inspect.getmembers(__import__(__name__), inspect.isclass)
-                  if issubclass(cls, UnitCategory) and cls is not UnitCategory]
+def get_categories():
+    return list((PressureUnitCategory, VolumeFlowRateUnitCategory, VolumeRationsUnitCategory))
 
-    # Выбор категории
+
+def choose_category(categories_classes):
     while True:
-        source_unit = None
-        target_unit = None
-        quantity = None
-
         print("Выберите категорию единиц измерения:")
-        for i, category in enumerate(categories, start=1):
+        for i, category in enumerate(categories_classes, start=1):
             print(f"{i}. {category.__name__}")
 
         try:
             category_choice = int(input("Введите номер категории: "))
-            if 1 <= category_choice <= len(categories):
-                selected_category = categories[category_choice - 1]
-                break
+            if 1 <= category_choice <= len(categories_classes):
+                return categories_classes[category_choice - 1]
             else:
                 print("Выберите правильную категорию.")
         except ValueError:
             print("Пожалуйста, введите корректное значение.")
 
+
+def choose_units(selected_category):
+    units = selected_category.get_units()
+    print("Выберите единицу измерения:")
+    for i, unit in enumerate(units, start=1):
+        print(f"{i}. {unit.symbol}")
+    return units
+
+
+def main():
+    source_unit = None
+    target_unit = None
+    values = None
+    categories_classes = get_categories()
+
+    selected_category = choose_category(categories_classes)
     print(f"Вы выбрали категорию: {selected_category.__name__}")
 
+    units = choose_units(selected_category)
+
     while True:
-        print("Выберите единицу измерения:")
-        units = selected_category.get_units()
-
-        for i, unit in enumerate(units, start=1):
-            print(f"{i}. {unit.symbol}")
-
         try:
             from_unit_value = int(input("Введите номер исходной единицы: "))
             if from_unit_value < 1 or from_unit_value > len(units):
@@ -55,13 +62,12 @@ def main():
                 print("Введите хотя бы одно значение.")
                 continue
 
-            quantity = values
             break
         except ValueError:
             print("Пожалуйста, введите корректное значение.")
 
-    result = UnitConverter.convert_unit(source_unit, target_unit, quantity)
-    output_result(source_unit, target_unit, quantity, result)
+    result = UnitConverter.convert_unit(source_unit, target_unit, values)
+    output_result(source_unit, target_unit, values, result)
 
 
 if __name__ == "__main__":
--- unit_converter.py
+++ unit_converter.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 @dataclass
-class UnitEnum:
-    r"""
+class UnitData:
+    """
     A base class for defining individual measurement units.
 
     Attributes
@@ -22,7 +22,7 @@ class UnitEnum:
 
 
 class UnitCategory:
-    r"""
+    """
     A base class for organizing categories of measurement units.
 
     This class serves as a foundation for defining specific categories of units
@@ -33,49 +33,50 @@ class UnitCategory:
 
     @classmethod
     def get_units(cls):
-        return [attr for attr in cls.__dict__.values() if isinstance(attr, UnitEnum)]
+        return [attr for attr in cls.__dict__.values() if isinstance(attr, UnitData)]
 
 
-class Pressure(UnitCategory):
-    r"""
+class PressureUnitCategory(UnitCategory):
+    """
     A unit category representing pressure measurements.
 
     This category includes common units of pressure and their conversion factors
-    relative to the base unit (Pascal). Examples include atmospheric pressure, bar, and psi.
+    relative to the base
+    unit (Pascal). Examples include atmospheric pressure, bar, and psi.
 
     Attributes
     ----------
-    PA : UnitEnum
+    PA : UnitData
         Pascal, the SI base unit of pressure.
-    HPA : UnitEnum
+    HPA : UnitData
         Hectopascal, equivalent to 100 pascals.
-    KPA : UnitEnum
+    KPA : UnitData
         Kilopascal, equivalent to 1,000 pascals.
-    MPA : UnitEnum
+    MPA : UnitData
         Megapascal, equivalent to 1,000,000 pascals.
-    AT : UnitEnum
+    AT : UnitData
         Technical atmosphere, used in engineering.
-    ATM : UnitEnum
+    ATM : UnitData
         Standard atmosphere, based on average sea-level pressure.
-    BAR : UnitEnum
+    BAR : UnitData
         Bar, equivalent to 100,000 pascals.
-    PSI : UnitEnum
+    PSI : UnitData
         Pounds per square inch, commonly used in the US.
 
     """
 
-    PA = UnitEnum(1, "Pa: pascal")
-    HPA = UnitEnum(100, "hPa: hectopascal")
-    KPA = UnitEnum(1000, "kPa: kilopascal")
-    MPA = UnitEnum(1000000, "MPa: megapascal")
-    AT = UnitEnum(98066.5, "at: technical atmosphere")
-    ATM = UnitEnum(101325, "atm: standard atmosphere")
-    BAR = UnitEnum(100000, "bar: bar")
-    PSI = UnitEnum(6894.76, "psi: pound-force per square inch")
+    PA = UnitData(1, "Pa: pascal")
+    HPA = UnitData(100, "hPa: hectopascal")
+    KPA = UnitData(1000, "kPa: kilopascal")
+    MPA = UnitData(1000000, "MPa: megapascal")
+    AT = UnitData(98066.5, "at: technical atmosphere")
+    ATM = UnitData(101325, "atm: standard atmosphere")
+    BAR = UnitData(100000, "bar: bar")
+    PSI = UnitData(6894.76, "psi: pound-force per square inch")
 
 
-class VolumeFlowRate(UnitCategory):
-    r"""
+class VolumeFlowRateUnitCategory(UnitCategory):
+    """
     A unit category representing volume flow rate measurements.
 
     This category defines commonly used units for flow rate, including barrels per day
@@ -83,58 +84,90 @@ class VolumeFlowRate(UnitCategory):
 
     Attributes
     ----------
-    BBL_D : UnitEnum
+    BBL_D : UnitData
         Barrel per day, a common unit in oil production.
-    MBBL_D : UnitEnum
+    MBBL_D : UnitData
         Thousand barrels per day.
-    MMBBL_D : UnitEnum
+    MMBBL_D : UnitData
         Million barrels per day.
-    BBL_Y : UnitEnum
+    BBL_Y : UnitData
         Barrel per year.
-    MBBL_Y : UnitEnum
+    MBBL_Y : UnitData
         Thousand barrels per year.
-    MMBBL_Y : UnitEnum
+    MMBBL_Y : UnitData
         Million barrels per year.
-    M3_D : UnitEnum
+    M3_D : UnitData
         Cubic meter per day.
-    E3M3_D : UnitEnum
+    E3M3_D : UnitData
         Thousand cubic meters per day.
-    E6M3_D : UnitEnum
+    E6M3_D : UnitData
         Million cubic meters per day.
-    SCF_D : UnitEnum
+    SCF_D : UnitData
         Standard cubic feet per day.
-    MSCF_D : UnitEnum
+    MSCF_D : UnitData
         Thousand standard cubic feet per day.
-    MMSCF_D : UnitEnum
+    MMSCF_D : UnitData
         Million standard cubic feet per day.
 
     """
 
-    BBL_D = UnitEnum(1, "bbl/d: barrel per day")
-    MBBL_D = UnitEnum(1000, "mbbl/d: thousand barrels per day")
-    MMBBL_D = UnitEnum(1000000, "mmbbl/d: million barrels per day")
-    BBL_Y = UnitEnum(0.00273791, "bbl/y: barrels per year")
-    MBBL_Y = UnitEnum(2.73791, "mbbl/y: thousand barrels per year")
-    MMBBL_Y = UnitEnum(2737.91, "mmbbl/y: million barrels per year")
-    M3_D = UnitEnum(6.28981, "m³/d: cubic meter per day")
-    E3M3_D = UnitEnum(6289.81, "E3m³/d: thousand cubic meter per day")
-    E6M3_D = UnitEnum(6289810.77, "E6m³/d: million cubic meter per day")
-    SCF_D = UnitEnum(0.1781, "scf/d: standard cubic feet per day")
-    MSCF_D = UnitEnum(178.1, "mscf/d: thousand standard cubic feet per day")
-    MMSCF_D = UnitEnum(178107.6, "mmscf/d: million standard cubic feet per day")
+    BBL_D = UnitData(1, "bbl/d: barrel per day")
+    MBBL_D = UnitData(1000, "mbbl/d: thousand barrels per day")
+    MMBBL_D = UnitData(1000000, "mmbbl/d: million barrels per day")
+    BBL_Y = UnitData(0.00273791, "bbl/y: barrels per year")
+    MBBL_Y = UnitData(2.73791, "mbbl/y: thousand barrels per year")
+    MMBBL_Y = UnitData(2737.91, "mmbbl/y: million barrels per year")
+    M3_D = UnitData(6.28981, "m³/d: cubic meter per day")
+    E3M3_D = UnitData(6289.81, "E3m³/d: thousand cubic meter per day")
+    E6M3_D = UnitData(6289810.77, "E6m³/d: million cubic meter per day")
+    SCF_D = UnitData(0.1781, "scf/d: standard cubic feet per day")
+    MSCF_D = UnitData(178.1, "mscf/d: thousand standard cubic feet per day")
+    MMSCF_D = UnitData(178107.6, "mmscf/d: million standard cubic feet per day")
+
+
+class VolumeRationsUnitCategory(UnitCategory):
+    """
+    A unit category representing volume-to-volume and gas-liquid ratio measurements.
+
+    This category defines commonly used units for representing ratios of volume to volume,
+    as well as standard gas-liquid ratio units such as standard cubic feet per stock tank barrel.
+
+    Attributes
+    ----------
+    M3_M3 : UnitData
+        Cubic meter per cubic meter, a dimensionless ratio commonly used in volume calculations.
+    SM3_SM3 : UnitData
+        Standard cubic meter per standard cubic meter, another dimensionless volume ratio.
+    SCF_STB : UnitData
+        Standard cubic feet per stock tank barrel, used to measure gas production relative to oil.
+    MSCF_STB : UnitData
+        Thousand standard cubic feet per stock tank barrel, a scaled version of SCF/STB.
+    STB_SCF : UnitData
+        Stock tank barrel per standard cubic foot, a reciprocal unit used in specific calculations.
+    STB_MSCF : UnitData
+        Stock tank barrel per thousand standard cubic feet, a scaled reciprocal of MSCF/STB.
+
+    """
+
+    M3_M3 = UnitData(1, "m³/m³: cubic meter per cubic meter")
+    SM3_SM3 = UnitData(1, "sm³/sm³: standard cubic meter per standard cubic meter")
+    SCF_STB = UnitData(0.17810760667903525, "scf/stb: standard cubic foot per stock tank barrel")
+    MSCF_STB = UnitData(178.10760667903526, "mscf/stb: thousand standard cubic feet per stock tank barrel")
+    STB_SCF = UnitData(5.614583333333334, "stb/scf: stock tank barrel per standard cubic foot")
+    STB_MSCF = UnitData(0.005614583333333333, "stb/mscf: stock tank barrel per thousand standard cubic feet")
 
 
 class UnitConverter:
     @staticmethod
-    def convert_unit(from_unit: UnitEnum, to_unit: UnitEnum, value: [float | Iterable[float]]) -> [float | np.ndarray]:
+    def convert_unit(from_unit: UnitData, to_unit: UnitData, value: [float | Iterable[float]]) -> [float | np.ndarray]:
         """
         Convert from one unit to another within a specified category.
 
         Parameters
         ----------
-        from_unit : UnitEnum
+        from_unit : UnitData
             The source unit.
-        to_unit : UnitEnum
+        to_unit : UnitData
             The target unit.
         value : float | Iterable[float]
             The value(s) to be converted.
@@ -151,26 +184,26 @@ class UnitConverter:
         return value * (from_unit.factor / to_unit.factor)
 
 
-def output_result(from_unit: UnitEnum, to_unit: UnitEnum, quantity: [float | Iterable[float]],
+def output_result(from_unit: UnitData, to_unit: UnitData, values: [float | Iterable[float]],
                   result: [float | np.ndarray]) -> None:
     """
     Outputs the results of unit conversion.
 
     Parameters
     ----------
-    from_unit : UnitEnum
+    from_unit : UnitData
         The unit being converted from.
-    to_unit : UnitEnum
+    to_unit : UnitData
         The unit being converted to.
-    quantity : float | Iterable[float]
-        The original quantity in the source unit.
+    values : float | Iterable[float]
+        The original values in the source unit.
     result : float | np.ndarray
-        The converted quantity in the target unit.
+        The converted values in the target unit.
 
     """
 
-    if isinstance(quantity, Iterable):
-        for q, r in zip(quantity, result):
+    if isinstance(values, Iterable):
+        for q, r in zip(values, result):
             print(f"{q} {from_unit.symbol} = {r} {to_unit.symbol}")
     else:
-        print(f"{quantity} {from_unit.symbol} = {result} {to_unit.symbol}")
+        print(f"{values} {from_unit.symbol} = {result} {to_unit.symbol}")
